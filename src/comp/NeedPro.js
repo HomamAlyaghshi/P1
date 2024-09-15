adaed7051207eff750681a3feffcbc3bed2f41f9
Homam Alyaghshi <homamalyaghshi@gmail.com>
2024-09-16 01:51:20 +0300

    f

--- src/comp/NeedPro.js
+++ src/comp/NeedPro.js
@@ -6,7 +6,7 @@ const NeedPro = () => {
       <div className="text-center mb-8">
         <h1 className="text-3xl font-bold text-gray-900">Need a Project?</h1>
       </div>
-      <form className="w-full max-w-4xl bg-white p-8 shadow-md rounded-lg">
+      <form className="w-full max-w-4xl ">
         <div className="flex flex-col space-y-6">
           <div className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-6">
             <div>
@@ -16,7 +16,7 @@ const NeedPro = () => {
                 name="name"
                 id="name"
                 autoComplete="given-name"
-                className="block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-cyan-500"
+                className="block w-full py-2 px-3  focus:outline-none focus:ring-2 focus:ring-cyan-500"
               />
             </div>
 
@@ -27,7 +27,7 @@ const NeedPro = () => {
                 name="email"
                 id="email"
                 autoComplete="email"
-                className="block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-cyan-500"
+                className="block w-full py-2 px-3  focus:outline-none focus:ring-2 focus:ring-cyan-500"
               />
             </div>
           </div>
@@ -39,7 +39,7 @@ const NeedPro = () => {
               name="title"
               type="text"
               autoComplete="title"
-              className="block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-cyan-500"
+              className="block w-full py-2 px-3  focus:outline-none focus:ring-2 focus:ring-cyan-500"
             />
           </div>
 
@@ -49,12 +49,12 @@ const NeedPro = () => {
               id="comment"
               name="comment"
               autoComplete="comment"
-              className="block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-cyan-500"
+              className="block w-full py-2 px-3 h-40  focus:outline-none focus:ring-2 focus:ring-cyan-500"
             />
           </div>
 
           <div className="flex justify-center">
-            <button className="bg-cyan-500 text-white py-2 px-6 rounded-md font-mono  focus:outline-none focus:ring-2 focus:ring-cyan-500 hover:bg-cyan-600 hover:text-xl hover:text-black transition-all duration-700">
+            <button className="bg-cyan-500 text-white py-2 px-6  font-mono  focus:outline-none focus:ring-2 focus:ring-cyan-500 hover:bg-cyan-600 hover:text-xl hover:text-black transition-all duration-700">
               Send Message
             </button>
           </div>
